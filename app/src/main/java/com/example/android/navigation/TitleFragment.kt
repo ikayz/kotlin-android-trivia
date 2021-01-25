ac0ce3449b5eaddeee4e73b6f1af212a3e0dfcd0
Isaac Miti <ikayz@live.com>
2021-01-25 23:54:09 +0200

    Add newer versions of the navigation dependencies

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -15,6 +15,9 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener { view: View ->
+
+        }
         return binding.root
     }
 }
